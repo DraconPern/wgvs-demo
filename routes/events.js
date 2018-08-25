73f6ed7f5c30720f7c5266a5992f414b63737b8b
DraconPern <draconpern@hotmail.com>
2018-08-24 19:59:17 -0500

    fill in the event webui

--- routes/events.js
+++ routes/events.js
@@ -9,6 +9,7 @@ router.get('/', function(req, res) {
     return res.render('events/index', agentbody);
   })
   .catch(function(err) {
+    req.flash('error', err);
     return res.render('events/index');
   })
 });
@@ -18,21 +19,61 @@ router.get('/new', function(req, res) {
 });
 
 router.post('/new', function(req, res, next) {
-
+  request.post(process.env.API_URL + '/api/events', { body: req.body, json: true})
+  .then(function(agentbody) {
+    req.flash('success', 'events created.');
+    return res.redirect('/events');
+  })
+  .catch(function(err) {
+    req.flash('error', err);
+    return res.render('events/new', req.body);
+  })
 })
 
 router.get('/:eventId', function(req, res, next) {
-    return res.render('events/update');
+  request(process.env.API_URL + '/api/events/' + req.params.eventId, {json: true})
+  .then(function(agentbody) {
+    return res.render('events/update', agentbody.event);
+  })
+  .catch(function(err) {
+    req.flash('error', err);
+    return res.render('events/update', req.body);
+  })
 })
 
 router.post('/:eventId', function(req, res, next) {
   if(req.body.submit == 'Update') {
-
+    updateEvent(req, res);
   } else if(req.body.submit == 'Delete') {
-
+    deleteEvent(req, res);
   } else {
     return res.redirect('/events');
   }
 })
 
+function updateEvent(req, res)
+{
+  request.post(process.env.API_URL + '/api/events/' + req.params.eventId, { body: req.body, json: true})
+  .then(function(agentbody) {
+    req.flash('success', 'Event updated.');
+    return res.redirect('/events');
+  })
+  .catch(function(err) {
+    req.flash('error', err);
+    return res.render('events/update', req.body);
+  });
+}
+
+function deleteEvent(req, res)
+{
+  request.delete(process.env.API_URL + '/api/events/' + req.params.eventId, { json: true})
+  .then(function(agentbody) {
+    req.flash('success', 'Event deleted.');
+    return res.redirect('/events');
+  })
+  .catch(function(err) {
+    req.flash('error', err);
+    return res.render('events/update', req.body);
+  });
+}
 module.exports = router;
