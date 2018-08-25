f8273a9ec22c04aef4e4cdc1cee2586c596195b9
DraconPern <draconpern@hotmail.com>
2018-08-24 19:58:52 -0500

    fill in api for events

--- routes/api/events.js
+++ routes/api/events.js
@@ -15,12 +15,44 @@ router.get('/', function(req, res, next) {
 })
 
 router.get('/:eventId', function(req, res, next) {
+  events.getEventAsync(req.params.eventId)
+  .then(function(event) {
+    return res.json({event});
+  })
+  .catch(function(err) {
+    return next(err);
+  })
 })
 
 router.post('/', function(req, res, next) {
+  events.addEventAsync(req.body)
+  .then(function(event) {
+    return res.json({event});
+  })
+  .catch(function(err) {
+    return next(err);
+  })
 })
 
 router.post('/:eventId', function(req, res, next) {
+  req.body.eventId = req.params.eventId;
+  events.updateEventAsync(req.body)
+  .then(function(event) {
+    return res.json(event);
+  })
+  .catch(function(err) {
+    return next(err);
+  })
+})
+
+router.delete('/:eventId', function(req, res, next) {
+  events.deleteEventAsync(req.params.eventId)
+  .then(function(result) {
+    return res.json(result);
+  })
+  .catch(function(err) {
+    return next(err);
+  })
 })
 
 module.exports = router;
